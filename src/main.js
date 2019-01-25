ee0791d53fb6e2f984a0a6c3f7691ea937adcf64
Jeffery Zhang <jeffery.zhang@sleepcare365.com>
2019-01-25 00:42:49 -0800

    use fastclick to cancle the 300ms delay when click button, div... on mobile end

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import 'babel-polyfill' //对一些es6的API进行转译
 import Vue from 'vue'
 import App from './App'
 
+//取消移动端点击300毫秒延迟
+import fastclick from 'fastclick'
 import 'common/stylus/index.styl'; // eslint-disable-line
+
+//将fastclick关联到dom的body上去这样的话, body下面按钮或者div等被点击的时候就不会有300毫秒的延迟了
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
