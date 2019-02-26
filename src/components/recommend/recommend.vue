5a4baf449c67cd00dc37d3e5ae11d472111610fe
Jeffery Zhang <jeffery.zhang@sleepcare365.com>
2019-02-26 11:29:32 -0800

    add loading indicator when loading image

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -26,11 +26,15 @@
           </ul>
         </div>
       </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Loading from "base/loading/loading";
 import Scroll from "base/scroll/scroll";
 import Slider from "base/slider/slider";
 import { getRecommend, getDiscList } from "api/recommend";
@@ -68,7 +72,8 @@ export default {
   },
   components: {
     Slider,
-    Scroll
+    Scroll,
+    Loading
   }
 };
 </script>
